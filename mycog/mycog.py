7c284f544d63fa7f1b128054d94fc0c223aa1b03
johannes-jansson <johannes.signup@gmail.com>
2018-03-30 09:21:50 +0200

    added rules for number of spies etc

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -2,6 +2,15 @@ import discord
 import random
 from discord.ext import commands
 
+spies = {}
+spies["5"] = 2
+spies["6"] = 2
+spies["7"] = 3
+spies["8"] = 3
+spies["9"] = 3
+spies["10"] = 4
+twofails = [7, 8, 9, 10]
+
 class Mycog:
     """My custom cog that does stuff!"""
 
@@ -11,14 +20,21 @@ class Mycog:
         self.settings["Players"] = {}
 
     @commands.command()
-    async def mycom(self):
-        """This does stuff!"""
+    async def init(self):
+        self.settings["Players"]["<@312534277524946945>"] = {"Name": user.name,
+                                                             "Mention": user.mention}
+        await self.bot.say("initialized")
 
-        #Your code will go here
+    @commands.command()
+    async def mycom(self):
         await self.bot.say("I can do stuff!")
 
     @commands.command()
     async def add_player(self, user : discord.Member):
+        print(user)
+        print(user.id)
+        print(user.name)
+        print(user.mention)
         self.settings["Players"][user.id] = {"Name": user.name,
                                              "Mention": user.mention}
         await self.bot.say("Added " + user.mention + " to the game.")
